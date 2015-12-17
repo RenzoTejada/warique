00a6375721651bc04cd70de61591b8e3c6e5436e
Renzo Tejada <renzzo09@gmail.com>
2015-12-17 02:52:34 -0500

    agregando al manifiesto los activitys

--- warique/app/src/main/java/com/example/putencio/warique/ReservarActivity.java
+++ warique/app/src/main/java/com/example/putencio/warique/ReservarActivity.java
@@ -3,16 +3,24 @@ package com.example.putencio.warique;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by renzo on 17/12/15.
  */
 public class ReservarActivity extends AppCompatActivity {
 
+    private TextView lblTexto;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservar);
+
+        lblTexto = (TextView) findViewById(R.id.lblTexto);
+
+        int posicion = getIntent().getIntExtra("posicion", -1);
+        lblTexto.setText("Posición->" + posicion);
     }
 
 
