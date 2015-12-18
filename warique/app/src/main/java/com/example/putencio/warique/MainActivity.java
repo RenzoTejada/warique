4227c8654ba65100dbb06e27a098a06771b2d088
Renzo Tejada <renzo.tejada@clicksandbricks.pe>
2015-12-18 16:52:46 -0500

    dandole colorcitos

--- warique/app/src/main/java/com/example/putencio/warique/MainActivity.java
+++ warique/app/src/main/java/com/example/putencio/warique/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.putencio.warique;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private Button btnReserva, btnEntrar;
 
